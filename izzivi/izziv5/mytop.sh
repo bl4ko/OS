730736103470ac760ac2e2e59e3165cb53348152
bl4ko <go3969@student.uni-lj.si>
2022-04-07 10:58:04 +0200

    working on izziv5

---
+++ izzivi/izziv5/mytop.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+comm="comm"
+mem="%mem"
+user="user"
+cpu="%cpu"
+
+
+while true
+do
+    clear
+    ps -eo pid,$comm,$mem,$user,$cpu | (sed -u 1q; sort -rnk 5) | head -11 | nl -v 0 | sed '1 s/0//'
+    read -t 1 -n 1 keypress
+
+    case $keypress in
+        "q")
+            exit 0
+        ;;
+        "h")
+            help
+            echo -e "q - izhod iz programa (quit)\nh - pomoc\n"
+        ;;
+        "p")
+            if test -n "$cpu"; then
+                cpu=""
+            else
+                cpu="%cpu"
+            fi
+        ;;
+        *)
+            continue
+        ;;
+    esac
+
+done
+
+
