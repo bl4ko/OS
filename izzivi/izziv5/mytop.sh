07e70af0e9ee0e77f47fbd4034ca6f4c9bd71462
bl4ko <go3969@student.uni-lj.si>
2022-04-07 19:37:01 +0200

    izziv 5 done

--- izzivi/izziv5/mytop.sh
+++ izzivi/izziv5/mytop.sh
@@ -1,30 +1,54 @@
 #!/bin/bash
 
-comm="comm"
-mem="%mem"
-user="user"
+comm="comm,"
+mem="%mem,"
+user="user,"
 cpu="%cpu"
 
 
 while true
 do
+
     clear
-    ps -eo pid,$comm,$mem,$user,$cpu | (sed -u 1q; sort -rnk 5) | head -11 | nl -v 0 | sed '1 s/0//'
+    ps -eo pid,$comm$mem$user$cpu | (sed -u 1q; sort -rnk 5) | head -11 | nl -v 0 | sed '1 s/0//'
     read -t 3 -n 1 keypress
+
+    if test "$keypress" = "h"; then
+        echo -e "q - izhod\nh - pomoc\nc - toggle command\nm - toggle memory\np - cpu toggle\nu - toggle user"
+        read -n 1 keypress
+
+    fi
 
     case $keypress in
         "q")
             exit 0
         ;;
-        "h")
-            help
-            echo -e "q - izhod iz programa (quit)\nh - pomoc\n"
+        "c")
+            if test -n "$comm"; then
+                comm=""
+            else
+                comm="comm,"
+            fi
+        ;;
+        "m")
+            if test -n "$mem"; then
+                mem=""
+            else
+                mem="%mem,"
+            fi
         ;;
         "p")
             if test -n "$cpu"; then
                 cpu=""
             else
                 cpu="%cpu"
+            fi
+        ;;
+        "u")
+            if test -n "$user"; then
+                user=""
+            else
+                user="user,"
             fi
         ;;
         *)
